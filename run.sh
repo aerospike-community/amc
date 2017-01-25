38bc9db6dd9b5208671428e0ac0ecaa9fbdd7ca4
Khosrow Afroozeh <parshua@gmail.com>
2017-01-25 06:11:14 +0100

    Added the run.sh script for easier development

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -x
+set -e
+
+cd $GOPATH/src/github.com/citrusleaf/amc
+
+edition=${1:-enterprise}
+environ=${2:-dev}
+
+build=`date -u +%Y%m%d.%H%M%S`
+version=`git describe --tags $(git rev-list --tags --max-count=1)`
+# tag=`git rev-parse --short HEAD`
+version_build="$edition-$version"
+
+# build binary
+go build -race -a -o amc -tags $edition -ldflags "-X github.com/citrusleaf/amc/common.AMCEdition=$edition -X github.com/citrusleaf/amc/common.AMCBuild=$build -X github.com/citrusleaf/amc/common.AMCVersion=$version -X github.com/citrusleaf/amc/common.AMCEnv=$environ"
+
+./amc -config-file=$GOPATH/src/github.com/citrusleaf/amc/amc.dev.conf
