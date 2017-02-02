032c94bfe2831e37cd94eedfb6c50f55f5c52f68
Khosrow Afroozeh <parshua@gmail.com>
2017-02-03 00:05:08 +0100

    Do not provide enterprise API for community build

--- controllers/main.go
+++ controllers/main.go
@@ -20,6 +20,8 @@ import (
 var (
 	_observer            *models.ObserverT
 	_defaultClientPolicy = as.NewClientPolicy()
+
+	registerEnterpriseAPI func(*echo.Echo)
 )
 
 func postSessionTerminate(c echo.Context) error {
@@ -149,14 +151,8 @@ func Server(config *common.Config) {
 	e.GET("/aerospike/service/debug", getDebug)
 	e.POST("/aerospike/service/clusters/:clusterUuid/debug", postDebug) // cluster does not matter here
 
-	e.GET("/alert-emails", sessionValidator(getAlertEmails))
-	e.POST("/alert-emails", sessionValidator(postAlertEmails))
-	e.POST("/delete-alert-emails", sessionValidator(deleteAlertEmails))
-
 	e.GET("/get_amc_version", getAMCVersion)
 	e.GET("/get_current_monitoring_clusters", sessionValidator(getCurrentMonitoringClusters))
-
-	e.GET("/aerospike/get_multicluster_view/:port", sessionValidator(getMultiClusterView))
 
 	e.POST("/set-update-interval/:clusterUuid", sessionValidator(setClusterUpdateInterval))
 	e.GET("/aerospike/service/clusters/:clusterUuid", sessionValidator(getCluster))
@@ -166,46 +162,19 @@ func Server(config *common.Config) {
 	e.POST("/aerospike/service/clusters/:clusterUuid/drop_udf", sessionValidator(postClusterDropUDF))
 	e.POST("/aerospike/service/clusters/:clusterUuid/add_udf", sessionValidator(postClusterAddUDF))
 
-	e.POST("/aerospike/service/clusters/:clusterUuid/fire_cmd", sessionValidator(postClusterFireCmd))
-	e.GET("/aerospike/service/clusters/:clusterUuid/get_all_users", sessionValidator(getClusterAllUsers))
-	e.GET("/aerospike/service/clusters/:clusterUuid/get_all_roles", sessionValidator(getClusterAllRoles))
-	e.POST("/aerospike/service/clusters/:clusterUuid/add_user", sessionValidator(postClusterAddUser))
-	e.POST("/aerospike/service/clusters/:clusterUuid/user/:user/remove", sessionValidator(postClusterDropUser))
-	e.POST("/aerospike/service/clusters/:clusterUuid/user/:user/update", sessionValidator(postClusterUpdateUser))
-	e.POST("/aerospike/service/clusters/:clusterUuid/roles/:role/add_role", sessionValidator(postClusterAddRole))
-	e.POST("/aerospike/service/clusters/:clusterUuid/roles/:role/update", sessionValidator(postClusterUpdateRole))
-	e.POST("/aerospike/service/clusters/:clusterUuid/roles/:role/drop_role", sessionValidator(postClusterDropRole))
-
-	e.POST("/aerospike/service/clusters/:clusterUuid/initiate_backup", sessionValidator(postInitiateBackup))
-	e.GET("/aerospike/service/clusters/:clusterUuid/get_backup_progress", sessionValidator(getBackupProgress))
-	e.GET("/aerospike/service/clusters/:clusterUuid/get_successful_backups", sessionValidator(getSuccessfulBackups))
-	e.POST("/aerospike/service/clusters/:clusterUuid/get_available_backups", sessionValidator(getAvailableBackups))
-
-	e.POST("/aerospike/service/clusters/:clusterUuid/initiate_restore", sessionValidator(postInitiateRestore))
-	e.GET("/aerospike/service/clusters/:clusterUuid/get_restore_progress", sessionValidator(getRestoreProgress))
-
 	e.GET("/aerospike/service/clusters/:clusterUuid/throughput", sessionValidator(getClusterThroughput))
 	e.GET("/aerospike/service/clusters/:clusterUuid/throughput_history", sessionValidator(getClusterThroughputHistory))
-	e.GET("/aerospike/service/clusters/:clusterUuid/latency/:nodes", sessionValidator(getNodeLatency))
-	e.GET("/aerospike/service/clusters/:clusterUuid/latency_history/:nodes", sessionValidator(getNodeLatencyHistory))
-	e.GET("/aerospike/service/clusters/:clusterUuid/nodes/:nodes/latency_history", sessionValidator(getNodesLatencyHistory))
 	e.GET("/aerospike/service/clusters/:clusterUuid/basic", sessionValidator(getClusterBasic))
-	e.POST("/aerospike/service/clusters/:clusterUuid/change_password", sessionValidator(postClusterChangePassword))
-	e.GET("/aerospike/service/clusters/:clusterUuid/alerts", sessionValidator(getClusterAlerts))
 	e.POST("/aerospike/service/clusters/:clusterUuid/add_node", sessionValidator(postAddClusterNodes))
 	e.GET("/aerospike/service/clusters/:clusterUuid/nodes/:nodes", sessionValidator(getClusterNodes))
 	e.GET("/aerospike/service/clusters/:clusterUuid/nodes/:node/allconfig", sessionValidator(getClusterNodeAllConfig))
 	e.POST("/aerospike/service/clusters/:clusterUuid/nodes/:nodes/setconfig", sessionValidator(setClusterNodesConfig))
 	e.POST("/aerospike/service/clusters/:clusterUuid/nodes/:node/switch_off", sessionValidator(postSwitchNodeOff))
-	e.POST("/aerospike/service/clusters/:clusterUuid/nodes/:node/switch_xdr_off", sessionValidator(postSwitchXDROff))
-	e.POST("/aerospike/service/clusters/:clusterUuid/nodes/:node/switch_xdr_on", sessionValidator(postSwitchXDROn))
 	e.GET("/aerospike/service/clusters/:clusterUuid/namespaces/:namespaces", sessionValidator(getClusterNamespaces))
 	e.GET("/aerospike/service/clusters/:clusterUuid/namespaces/:namespace/nodes/:nodes", sessionValidator(getClusterNamespaceNodes))
 	e.GET("/aerospike/service/clusters/:clusterUuid/namespaces/:namespace/nodes/:node/allconfig", sessionValidator(getClusterNamespaceAllConfig))
 	e.POST("/aerospike/service/clusters/:clusterUuid/namespaces/:namespace/nodes/:node/setconfig", sessionValidator(setClusterNamespaceConfig))
-	e.GET("/aerospike/service/clusters/:clusterUuid/xdr/:xdrPort/nodes/:nodes", sessionValidator(getClusterXdrNodes))
-	e.GET("/aerospike/service/clusters/:clusterUuid/xdr/:xdrPort/nodes/:nodes/allconfig", sessionValidator(getClusterXdrNodesAllConfig))
-	e.POST("/aerospike/service/clusters/:clusterUuid/xdr/:xdrPort/nodes/:nodes/setconfig", sessionValidator(setClusterXdrNodesConfig))
+
 	e.GET("/aerospike/service/clusters/:clusterUuid/nodes/:node/allstats", sessionValidator(getClusterNodeAllStats))
 	e.GET("/aerospike/service/clusters/:clusterUuid/namespaces/:namespace/nodes/:node/allstats", sessionValidator(getClusterNamespaceNodeAllStats))
 	e.GET("/aerospike/service/clusters/:clusterUuid/xdr/:port/nodes/:node/allstats", sessionValidator(getClusterXdrNodeAllStats))
@@ -221,8 +190,10 @@ func Server(config *common.Config) {
 	e.GET("/aerospike/service/clusters/:clusterUuid/jobs/nodes/:node", getClusterJobsNode)
 
 	e.POST("/aerospike/service/clusters/get-cluster-id", postGetClusterId)
-	e.GET("/aerospike/service/clusters/:clusterUuid/get-current-user", sessionValidator(getClusterCurrentUser))
-	e.GET("/aerospike/service/clusters/:clusterUuid/get_user_roles", sessionValidator(getClusterUserRoles))
+
+	if registerEnterpriseAPI != nil {
+		registerEnterpriseAPI(e)
+	}
 
 	log.Infof("Starting AMC server, version: %s %s", common.AMCVersion, common.AMCEdition)
 	// Start server
---
+++ controllers/main_enterprise.go
@@ -0,0 +1,49 @@
+// +build enterprise
+
+package controllers
+
+import "github.com/labstack/echo"
+
+func registerEnterprise(e *echo.Echo) {
+	e.GET("/aerospike/service/clusters/:clusterUuid/get-current-user", sessionValidator(getClusterCurrentUser))
+	e.GET("/aerospike/service/clusters/:clusterUuid/get_user_roles", sessionValidator(getClusterUserRoles))
+
+	e.POST("/aerospike/service/clusters/:clusterUuid/initiate_backup", sessionValidator(postInitiateBackup))
+	e.GET("/aerospike/service/clusters/:clusterUuid/get_backup_progress", sessionValidator(getBackupProgress))
+	e.GET("/aerospike/service/clusters/:clusterUuid/get_successful_backups", sessionValidator(getSuccessfulBackups))
+	e.POST("/aerospike/service/clusters/:clusterUuid/get_available_backups", sessionValidator(getAvailableBackups))
+
+	e.POST("/aerospike/service/clusters/:clusterUuid/initiate_restore", sessionValidator(postInitiateRestore))
+	e.GET("/aerospike/service/clusters/:clusterUuid/get_restore_progress", sessionValidator(getRestoreProgress))
+
+	e.GET("/alert-emails", sessionValidator(getAlertEmails))
+	e.POST("/alert-emails", sessionValidator(postAlertEmails))
+	e.POST("/delete-alert-emails", sessionValidator(deleteAlertEmails))
+
+	e.GET("/aerospike/get_multicluster_view/:port", sessionValidator(getMultiClusterView))
+
+	e.POST("/aerospike/service/clusters/:clusterUuid/fire_cmd", sessionValidator(postClusterFireCmd))
+	e.GET("/aerospike/service/clusters/:clusterUuid/get_all_users", sessionValidator(getClusterAllUsers))
+	e.GET("/aerospike/service/clusters/:clusterUuid/get_all_roles", sessionValidator(getClusterAllRoles))
+	e.POST("/aerospike/service/clusters/:clusterUuid/add_user", sessionValidator(postClusterAddUser))
+	e.POST("/aerospike/service/clusters/:clusterUuid/user/:user/remove", sessionValidator(postClusterDropUser))
+	e.POST("/aerospike/service/clusters/:clusterUuid/user/:user/update", sessionValidator(postClusterUpdateUser))
+	e.POST("/aerospike/service/clusters/:clusterUuid/roles/:role/add_role", sessionValidator(postClusterAddRole))
+	e.POST("/aerospike/service/clusters/:clusterUuid/roles/:role/update", sessionValidator(postClusterUpdateRole))
+	e.POST("/aerospike/service/clusters/:clusterUuid/roles/:role/drop_role", sessionValidator(postClusterDropRole))
+
+	e.GET("/aerospike/service/clusters/:clusterUuid/latency/:nodes", sessionValidator(getNodeLatency))
+	e.GET("/aerospike/service/clusters/:clusterUuid/latency_history/:nodes", sessionValidator(getNodeLatencyHistory))
+	e.GET("/aerospike/service/clusters/:clusterUuid/nodes/:nodes/latency_history", sessionValidator(getNodesLatencyHistory))
+	e.POST("/aerospike/service/clusters/:clusterUuid/change_password", sessionValidator(postClusterChangePassword))
+	e.GET("/aerospike/service/clusters/:clusterUuid/alerts", sessionValidator(getClusterAlerts))
+	e.POST("/aerospike/service/clusters/:clusterUuid/nodes/:node/switch_xdr_off", sessionValidator(postSwitchXDROff))
+	e.POST("/aerospike/service/clusters/:clusterUuid/nodes/:node/switch_xdr_on", sessionValidator(postSwitchXDROn))
+	e.GET("/aerospike/service/clusters/:clusterUuid/xdr/:xdrPort/nodes/:nodes", sessionValidator(getClusterXdrNodes))
+	e.GET("/aerospike/service/clusters/:clusterUuid/xdr/:xdrPort/nodes/:nodes/allconfig", sessionValidator(getClusterXdrNodesAllConfig))
+	e.POST("/aerospike/service/clusters/:clusterUuid/xdr/:xdrPort/nodes/:nodes/setconfig", sessionValidator(setClusterXdrNodesConfig))
+}
+
+func init() {
+	registerEnterpriseAPI = registerEnterprise
+}
