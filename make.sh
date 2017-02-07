a3d74550b1bbdd630a1996dacc5b85b792abb092
Khosrow Afroozeh <parshua@gmail.com>
2017-02-07 18:39:57 +0100

    Another go at cross compilation for all diff. linux platforms

--- make.sh
+++ make.sh
@@ -10,6 +10,9 @@ environ=$2
 platform=${3:-linux}
 sysname=$(uname | tr '[:upper:]' '[:lower:]')
 
+maintainer="Khosrow Afroozeh (khosrow@aerospike.com)"
+description="Aerospike Management Console"
+
 echo "platform is ${platform}"
 echo "sysname is ${sysname}"
 
@@ -19,7 +22,7 @@ version=`git describe --tags $(git rev-list --tags --max-count=1)`
 version_build="$edition-$version"
 
 # build binary
-GOOS=$platform go build -a -tags $edition -ldflags "-X github.com/citrusleaf/amc/common.AMCEdition=$edition -X github.com/citrusleaf/amc/common.AMCBuild=$build -X github.com/citrusleaf/amc/common.AMCVersion=$version -X github.com/citrusleaf/amc/common.AMCEnv=$environ" -o deployment/release/amc/opt/amc/amc .
+CGO_ENABLED=0 GOOS=$platform go build -a -tags "purego $edition" -ldflags "-X github.com/citrusleaf/amc/common.AMCEdition=$edition -X github.com/citrusleaf/amc/common.AMCBuild=$build -X github.com/citrusleaf/amc/common.AMCVersion=$version -X github.com/citrusleaf/amc/common.AMCEnv=$environ" -o deployment/release/amc/opt/amc/amc .
 
 # build content
 rm -rf build/static
@@ -40,20 +43,17 @@ cp -R mailer/templates/ deployment/release/amc/opt/amc/mailer/
 
 case $platform in
 	'linux')
-		rm -f *.rpm
-		rm -f *.deb
-
 		rm -f deployment/release/amc/etc/init.d/*
 		cp -f deployment/common/amc.rpm deployment/release/amc/etc/init.d/amc
 		chmod +x deployment/release/amc/etc/init.d/amc
-		fpm -s dir -t rpm -n "aerospike-amc-$edition" -v $version -C deployment/release/amc .
+		fpm -f -s dir -t rpm -n "aerospike-amc-$edition" -v $version -C deployment/release/amc -m "$maintainer" --description "$description" .
 
 		rm -f deployment/release/amc/etc/init.d/*
 		cp -f deployment/common/amc.deb deployment/release/amc/etc/init.d/amc
 		chmod +x deployment/release/amc/etc/init.d/amc
-		fpm -s dir -t deb -n "aerospike-amc-$edition" -v $version -C deployment/release/amc .
+		fpm -f -s dir -t deb -n "aerospike-amc-$edition" -v $version -C deployment/release/amc  -m "$maintainer" --description "$description" .
 		;;
-	'dawin')
+	'darwin')
 		;;
 	*)
 		echo "unrecognized platform ${platform}"
