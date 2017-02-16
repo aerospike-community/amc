3fb97a1146023d146d4d780cd417fbad591bb6f6
Khosrow Afroozeh <parshua@gmail.com>
2017-02-16 16:59:10 +0100

    do not add commit hash to the package file name

--- make.sh
+++ make.sh
@@ -19,14 +19,14 @@ echo "sysname is ${sysname}"
 
 build=`date -u +%Y%m%d.%H%M%S`
 version=`git describe --tags $(git rev-list --tags --max-count=1)`
+
+amc_version=version
 if [ $release != "release" ];	then
-	version=`git describe --tags`;
+	amc_version=`git describe --tags`;
 fi
 
-version_build="$edition-$version"
-
 # build binary
-CGO_ENABLED=0 GOOS=$platform go build -a -tags "purego $edition" -ldflags "-X github.com/citrusleaf/amc/common.AMCEdition=$edition -X github.com/citrusleaf/amc/common.AMCBuild=$build -X github.com/citrusleaf/amc/common.AMCVersion=$version -X github.com/citrusleaf/amc/common.AMCEnv=$environ" -o deployment/release/amc/opt/amc/amc .
+CGO_ENABLED=0 GOOS=$platform go build -a -tags "purego $edition" -ldflags "-X github.com/citrusleaf/amc/common.AMCEdition=$edition -X github.com/citrusleaf/amc/common.AMCBuild=$build -X github.com/citrusleaf/amc/common.AMCVersion=$amc_version -X github.com/citrusleaf/amc/common.AMCEnv=$environ" -o deployment/release/amc/opt/amc/amc .
 
 # build content
 rm -rf build/static
@@ -50,12 +50,12 @@ case $platform in
 		rm -f deployment/release/amc/etc/init.d/*
 		cp -f deployment/common/amc.rpm deployment/release/amc/etc/init.d/amc
 		chmod +x deployment/release/amc/etc/init.d/amc
-		fpm -f -s dir -t rpm -n "aerospike-amc-$edition" -v $version -C deployment/release/amc -m "$maintainer" --description "$description" .
+		fpm -f -s dir -t rpm -n "aerospike-amc-$edition" -v $version -C deployment/release/amc -m "$maintainer" --description "$description" --vendor "Aerospike" .
 
 		rm -f deployment/release/amc/etc/init.d/*
 		cp -f deployment/common/amc.deb deployment/release/amc/etc/init.d/amc
 		chmod +x deployment/release/amc/etc/init.d/amc
-		fpm -f -s dir -t deb -n "aerospike-amc-$edition" -v $version -C deployment/release/amc  -m "$maintainer" --description "$description" .
+		fpm -f -s dir -t deb -n "aerospike-amc-$edition" -v $version -C deployment/release/amc  -m "$maintainer" --description "$description" --vendor "Aerospike" .
 		;;
 	'darwin')
 		;;
