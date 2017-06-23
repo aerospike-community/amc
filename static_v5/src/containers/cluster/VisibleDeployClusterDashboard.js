55404e60323a4c367780e6d0730ea87fa2b80401
Hemant Patre <hemant@aerospike.com>
2017-06-23 07:23:48 +0530

    Added VisibleDeployClusterDashboard for deploy cluster dashboard.

---
+++ static_v5/src/containers/cluster/VisibleDeployClusterDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import DeployClusterDashboard from 'components/cluster/DeployClusterDashboard';
+
+const VisibleDeployClusterDashboard = connect(
+  null,
+  null
+)(DeployClusterDashboard);
+
+export default VisibleDeployClusterDashboard;
+
+
