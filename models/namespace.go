fbe079ac226cb6625e8ff4d20f974bed3a53b6d2
Khosrow Afroozeh <parshua@gmail.com>
2018-04-06 20:27:54 +0200

    Fix replication-factor issue. #TOOLS-1120

--- models/namespace.go
+++ models/namespace.go
@@ -338,6 +338,8 @@ func (ns *Namespace) setAliases() {
 	calcStats["write-block-size"] = stats.TryInt("storage-engine.write-block-size", 0)
 	calcStats["sub-objects"] = stats.TryInt("sub_objects", 0)
 
+	calcStats["repl-factor"] = stats.TryInt("replication-factor", stats.TryInt("repl-factor", 0))
+
 	ns.calcStats.SetStats(calcStats)
 }
 
